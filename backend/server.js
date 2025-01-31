84c7d11067599e7b530cf3bb5c831d5377baa85b
CarlosBCIII <88202826+CarlosBCIII@users.noreply.github.com>
2025-01-31 14:59:09 -0300

    ajuste no nome das rotas e paths

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,9 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cloudinary from "cloudinary";
 
-import authRoutes from "./routes/auth.routes.js";
-import userRoutes from "./routes/user.routes.js";
-import postRoutes from "./routes/post.routes.js";
+import authRoute from "./routes/auth.route.js";
+import userRoute from "./routes/user.route.js";
+import postRoute from "./routes/post.route.js";
 
 import connectMongoDB from "./db/connectMongo.js"
 
@@ -25,9 +25,9 @@ app.use(express.urlencoded({extended:true}));
 
 app.use(cookieParser());
 
-app.use("/api/auth", authRoutes);
-app.use("/api/users", userRoutes);
-app.use("/api/post", postRoutes);
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/post", postRoute);
 
 
 app.listen(PORT, () =>{
